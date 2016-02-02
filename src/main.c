b7d088b2db6ea5ee1acac4c568ffa810b51a3aef
lkrieg <leonkrieg93@gmail.com>
2016-02-02 18:20:57 +0100

    Added return statements in main()

--- src/main.c
+++ src/main.c
@@ -16,14 +16,14 @@ int main(int argc, char** argv) {
 	/* check for file parameter */
 	if (argc != 2) {
 		printf("Usage: parser FILENAME\n");
-		exit(1);
+		return 1;
 	}
 
 	/* open file stream */
 	FILE* file = fopen(argv[1], "r");
 	if (file == NULL) {
 		printf("Could not open file.");
-		exit(1);
+		return 1;
 	}
 
 	/* parse from stream */
